ce9b499aac6d6eabb8c0d909e1497538d0e1c8cc
Nick Lee <leexnick@gmail.com>
2019-05-05 22:39:25 -0700

    final update before deploy

--- server.js
+++ server.js
@@ -1,16 +1,62 @@
-const express = require('express')
-const morgan = require('morgan')
+'use strict';
 
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
-app.use(morgan('dev'))
+const app = express();
 
-app.use((req, res) => {
-  res.send('Hello, world!')
-})
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev';
+app.use(morgan(morganSetting));
 
-const PORT = 8000
+const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychich', 'Rock', 'Steel', 'Water'];
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({error: 'Unauthorized request'});
+  }
+  
+  next();
+});
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  const { name, type } = req.query;
+  let pokemons = POKEDEX.pokemon;
+
+  if (name) {
+    pokemons = pokemons.filter( pokemon => pokemon.name.toLowerCase().includes(name.toLowerCase()) );
+  }
+
+  if (type) {
+    pokemons = pokemons.filter( pokemon => pokemon.type.includes(type) );
+  }
+
+  res
+    .status(200)
+    .json(pokemons);
+}
+
+app.get('/pokemon', handleGetPokemon);
+
+app.use((error, req, res, next) => {
+  let response;
+  if(process.env.NODE_ENV === 'produciton') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
